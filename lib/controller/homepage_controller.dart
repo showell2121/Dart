cb2584ca4c9c8619358bb9e8345459c41bfbf113
showell2121 <showell2121@gmail.com>
2020-03-19 09:54:05 -0500

    finished popup boxes

--- lib/controller/homepage_controller.dart
+++ lib/controller/homepage_controller.dart
@@ -15,11 +15,18 @@ class HomePageController {
     showDialog(
       context: homepage.context,
       builder: (BuildContext context){
+        //alert user with pop up box
         return AlertDialog(
           title: Text('Flat Button'),
           content: Text("Flat Button is pressed"),
           actions: <Widget>[
-            Ra
+            FlatButton(
+              child: Text('OK', ), //style: TextStyle(color: Colors.white,),
+              //on press navigates to homepage.context
+              onPressed: (){
+              Navigator.pop(homepage.context);
+              },
+            ), 
           ],
         );
       },
@@ -28,10 +35,50 @@ class HomePageController {
   }
 
   void iconButtonPressed(){
+    //allows popup alert
+    showDialog(
+      context: homepage.context,
+      builder: (context){
+        //alert user with pop up box
+        return AlertDialog(
+          title: Text('Icon Button'),
+          content: Text("Icon Button is pressed"),
+          actions: <Widget>[
+            IconButton(
+              //homepage for icon uses icon: instead of child:
+              icon: Text('OK', ), //style: TextStyle(color: Colors.white,),
+              onPressed: (){
+              Navigator.pop(homepage.context);
+              },
+            ), 
+          ],
+        );
+      },
+    );
     print("Icon Button Pressed");
   }
 
   void raisedButtonPressed(){
+
+    //allows popup alert
+    showDialog(
+      context: homepage.context,
+      builder: (BuildContext context){
+        //alert user with pop up box
+        return AlertDialog(
+          title: Text('Raised Button'),
+          content: Text("Raised Button is pressed"),
+          actions: <Widget>[
+            RaisedButton(
+              child: Text('OK', ), //style: TextStyle(color: Colors.white,),
+              onPressed: (){
+              Navigator.pop(homepage.context);
+              },
+            ), 
+          ],
+        );
+      },
+    );    
     print("Raised Button Pressed");
   }
   
