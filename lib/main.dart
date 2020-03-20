9a86f9b0018dea4e1ed218431d8452916b66fe1a
showell2121 <showell2121@gmail.com>
2020-03-20 13:06:08 -0500

    Lesson2

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+//using material design in this package. 
+import 'package:flutter/material.dart';
+
+
+//Main program of dart
+void main(){
+
+  //this is the app that is run and the first function
+  runApp(Lesson2App());
+}
+
+//This project will extend statless widget and allow changes on same page. 
+class Lesson2App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return null;
+  }
+
+
+
+}
+
+
